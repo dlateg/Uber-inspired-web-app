04a0d4c5281f983daf65c8cf00feadad554a5c36
dlateg <lategoladenis@gmail.com>
2023-06-20 20:46:11 +0100

    debugging map changes

--- static/map.js
+++ static/map.js
@@ -4,22 +4,28 @@ function initMap() {
         center: {lat: 51.5195786, lng: -0.0606907}
     });
 
-    var vehiclesData = document.getElementById('vehicles-data').value;
-    var vehicles = JSON.parse(vehiclesData);
-    
+    // Get the vehicles data from the hidden input field
+    var vehicles = document.getElementById('vehicles-data').value;
+    var vehicles = JSON.parse(vehicles);
+
     // Array to store markers
-    var markers = []; 
+    var markers = [];
+    const image = "https://apollo-media.codio.co.uk/media%2F1%2F9a9ed3e3e1ee96baf9da3e47d4570c5c-0e41f5b92e883928.webp";
 
     for (var i = 0; i < vehicles.length; i++) {
         var vehicle = vehicles[i];
         var marker = new google.maps.Marker({
             position: {lat: vehicle.latitude, lng: vehicle.longitude},
             map: map,
-            icon: 'car.webp',
-            title: vehicle.place
+            icon: image
         });
-       // Store the marker in the array
-        markers.push(marker); 
-    }
 
+        // Store the marker in the array
+        markers.push(marker);
+    }
+}
+
+// Call the initMap function when the Google Maps script is loaded
+function loadMap() {
+    initMap();
 }
